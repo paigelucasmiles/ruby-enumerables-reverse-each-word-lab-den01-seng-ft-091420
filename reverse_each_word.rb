1b192131462c2ed23188d0a3db73c99d4f892740
Paige Miles <paige@lucas-miles.com>
2020-09-07 15:53:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ require 'pry'
 def reverse_each_word(string)
 array = string.split
   array_letters = []
-  array.each do |element|
+  array.collect do |element|
     array_letters.push(element.chars)
   end
   reversed_nested_array = []
